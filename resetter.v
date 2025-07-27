68dc615bb05cb192fbfd5ac894b1681c44b5d232
iewGNOT <128372645+iewGNOT@users.noreply.github.com>
2025-07-27 17:22:12 +0800

    Update resetter.v

--- resetter.v
+++ resetter.v
@@ -1,12 +1,12 @@
 /1
 
 module resetter #(
-    parameter integer PULSE_CYCLES = 5_000_000  // 50 ms @ 100 MHz
+    parameter integer PULSE_CYCLES = 5_000_000  // 50 ms @ 100 MHz，可按需调整
 )(
     input  wire clk,
-    input  wire enable,
-    output reg  reset_line,   // 低有效：0=复位中 (接芯片 nRESET)
-    output reg  wide_glitch   // 高有效：1=复位中 (并到 power_ctrl)
+    input  wire enable,       // 高电平触发一次宽脉冲
+    output reg  reset_line,   // 低有效：0=复位中（接 nRESET）
+    output reg  wide_glitch   // 高有效：1=断电（并到 power_ctrl）
 );
     reg [31:0] cnt = 0;
     always @(posedge clk) begin
@@ -15,7 +15,7 @@ module resetter #(
         end else if (cnt != 0) begin
             cnt <= cnt - 1'b1;
         end
-        reset_line <= (cnt != 0) ? 1'b0 : 1'b1;  // nRESET 低有效
-        wide_glitch <= (cnt != 0) ? 1'b1 : 1'b0; // power_ctrl 高有效
+        reset_line  <= (cnt != 0) ? 1'b0 : 1'b1;
+        wide_glitch <= (cnt != 0) ? 1'b1 : 1'b0;
     end
 endmodule
